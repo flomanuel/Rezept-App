d03ea37e591fce7f02f5d4fe16d1bed328cafb54
Baldorim <Soul_Eater1@gmx.de>
2020-01-29 15:00:30 +0100

    entities

--- src/app/entity/recipe.ts
+++ src/app/entity/recipe.ts
@@ -1,11 +1,18 @@
 import { Category } from './category.enum';
-import { Ingrediant } from './ingrediant';
 import { Region } from './region.enum';
+import { Ingredient } from './ingredient';
 
 export class Recipe {
   public missingIngredients: number;
 
-  constructor(public ID: number, public title: string, public preparationTime: number, public category: Category[], public region: Region[], public ingredients: Ingrediant[], public instructions: string, public image: string) {
+  constructor(public ID: number,
+              public title: string,
+              public preparationTime: number,
+              public category: Category[],
+              public region: Region[],
+              public ingredients: Ingredient[],
+              public instructions: string,
+              public image: string) {
   }
 
   public add_missing(): void {
