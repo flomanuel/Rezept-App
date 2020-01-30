053ceddeb371693209e18dcb1d3f2afbefa10a5e
Hydoc <felixsehnert@yahoo.de>
2020-01-30 13:22:02 +0100

    -> Zutaten Klasse

--- src/app/entity/ingredient.class.ts
+++ src/app/entity/ingredient.class.ts
@@ -3,14 +3,10 @@ export class Ingredient {
   private readonly _amount: number;
   private readonly _suffix: string;
 
-  private constructor(label: string, amount: number, suffix: string) {
+  constructor(label: string, amount: number, suffix: string) {
     this._label = label;
     this._amount = amount;
     this._suffix = suffix;
-  }
-
-  static create(label: string, amount: number, suffix: string): Ingredient {
-    return new Ingredient(label, amount, suffix);
   }
 
   get label(): string {
