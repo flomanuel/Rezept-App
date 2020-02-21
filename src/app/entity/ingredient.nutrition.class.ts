bfb2031e7fd3105e4f3b5d5ae24231cd63ee0170
Florian Sauer <floe.sauer@googlemail.com>
2020-02-21 10:03:12 +0100

    changed classes to interfaces

--- src/app/entity/ingredient.nutrition.class.ts
+++
@@ -1,4 +0,0 @@
-export class IngredientNutritionClass {
-  constructor(public carbohydrates: number, public fat: number, public kcal: number, public protein: number, public roughage: number) {
-  }
-}
