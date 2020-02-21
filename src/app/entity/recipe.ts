9369ce3760ee3767fdde2267a736e32fd5841ccf
Florian Sauer <floe.sauer@googlemail.com>
2020-02-21 11:53:29 +0100

    refactored ingredient entity, added "customTitle" to ingredient entity

--- src/app/entity/recipe.ts
+++ src/app/entity/recipe.ts
@@ -13,8 +13,8 @@ import { RecipeStep } from './RecipeStep';
 export class Recipe extends Tag {
   public missingIngredients: number;
 
-  constructor(public id: Id,
-              public title: Title,
+  constructor(id: Id,
+              title: Title,
               public preparationTime: PreparationTime,
               public category: Category[],
               public region: Region[],
