2b8ab379d48a3533873dfff0713c4821ba5dacc9
Florian Sauer <floe.sauer@googlemail.com>
2020-04-29 20:24:10 +0200

    recipe list element: implement actual count of missing ingredients

--- src/app/components/_search-page/recipe-list-element/recipe-list-element.component.ts
+++ src/app/components/_search-page/recipe-list-element/recipe-list-element.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Recipe } from '../../../entity/recipe';
+import { Ingredient } from '../../../entity/ingredient.class';
+import { FridgeService } from '../../../services/fridge.service';
+import { DefaultIngredientService } from '../../../services/default-ingredient.service';
 
 @Component({
   selector: 'app-recipe-list-element',
@@ -11,14 +14,31 @@ export class RecipeListElementComponent implements OnInit {
 
   private fallbackImagePath = '../../../../assets/objects/fallbackImage.svg';
 
-  constructor() {
+  constructor(private fridgeService: FridgeService, private defaultIngredientService: DefaultIngredientService) {
   }
 
   ngOnInit() {
   }
 
-  countMissingIngredients(): number {
-    return 5;
+
+  get amountMissingIngredients(): number {
+    let missing = 0;
+    this.recipe.ingredients.forEach((ingredient) => {
+      if (!this.isIngredientAvailable(ingredient)) {
+        missing++;
+      }
+    });
+    return missing;
+  }
+
+
+  isIngredientAvailable(ingredient: Ingredient): boolean {
+    const availabilityStatus = this.fridgeService.isIngredientInFridge(
+      ingredient) || this.defaultIngredientService.isIngredientDefaultIngredient(ingredient,
+    );
+    // todo: implement check in shopping list to only display ingredients that are not available
+    ingredient.done = availabilityStatus;
+    return availabilityStatus;
   }
 
   calculatePreparationTime(time: number): number[] {
