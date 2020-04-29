7e5a580eeab19cc8b9dcac5416989663f524bcfd
Florian Sauer <floe.sauer@googlemail.com>
2020-04-29 19:23:23 +0200

    fridge: change default value for "saved" to false
    Since on fridge detail page you need to save the ingredient at lease once.
    And if the form elements for inputting and saving data are hidden (which they are for saved === true), the user thinks that the fridge ingredients get saved by default.

--- src/app/entity/ingredient.class.ts
+++ src/app/entity/ingredient.class.ts
@@ -2,6 +2,6 @@ export class Ingredient {
 
   constructor(public label: string, public amount: number, public volumeUnit: string, public id: number,
               public customTitle: string = '', public additionalInfo: boolean = false, public done: boolean = false,
-              public saved: boolean = true) {
+              public saved: boolean = false) {
   }
 }
